907c716e170ed28d3d2115c3c10d1a79131b8f55
lnourry75 <90520293+lnourry75@users.noreply.github.com>
2022-10-11 16:04:46 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // On définit les constantes 
 const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({ // On a un schema user avec un username et password
     username: { type: String, required: true, unique: true},
     password: { type: String, required: true },
   });
   
-  userSchema.plugin(uniqueValidator);
+  userSchema.plugin(uniqueValidator); // Faire en sorte qu’on puisse pas mettre 2 fois le même nom d’utilisateur
 
   module.exports = mongoose.model('User', userSchema);

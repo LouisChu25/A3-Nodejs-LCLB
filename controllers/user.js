95a9926f38f4da29b4c525ee1efbc1171b6ec2f3
lnourry75 <90520293+lnourry75@users.noreply.github.com>
2022-10-11 16:06:20 +0200

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,16 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // On définit les constantes 
 const User = require('../models/User');
 
 
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res, next) => { // Fonction pour créer un utilisateur avec un username et un mot de passe haché
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
         const user = new User({
           username: req.body.username,
           password: hash
         });
-        user.save()
+        user.save()  // Fonction pour sauvegarder un utilisateur
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
           .catch(error => res.status(400).json({ error }));
       })
@@ -18,13 +18,13 @@ exports.signup = (req, res, next) => {
   };
 
 
-  exports.login = (req, res, next) => {
+  exports.login = (req, res, next) => { // Fonction pour retrouver l'utilisateur dans la base de donnés 
     User.findOne({ username: req.body.username })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
             }
-            bcrypt.compare(req.body.password, user.password)
+            bcrypt.compare(req.body.password, user.password)  // On compare le mot de passe saisi et celui en base de donnée 
                 .then(valid => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });

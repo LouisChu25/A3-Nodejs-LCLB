4e076fb3764bc2047650b43a8bf6372a62afe55a
lnourry75 <90520293+lnourry75@users.noreply.github.com>
2022-10-11 16:02:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, res, next) => {  // Pour éviter les erreurs de CORS 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
